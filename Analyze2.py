9c0033d5e6b9476290425b7ce4907656b469e93e
kohankhaki <kohankha@ualberta.ca>
2021-02-16 16:17:02 -0700

    Analyze 2 added.

---
+++ Analyze2.py
@@ -0,0 +1,98 @@
+import numpy as np
+import random
+from matplotlib import pyplot as plt
+
+
+def generate_hex_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    c = (r, g, b)
+    hex_c = '#%02x%02x%02x' % c
+    return hex_c
+
+
+def drawPlotUncertainty(x, y, y_err, label, color, axis):
+    axis.plot(x, y, color, label=label)
+    axis.fill_between(x,
+                      y - y_err,
+                      y + y_err,
+                      facecolor=color, alpha=.4, edgecolor='none')
+
+
+def plot_simple_agent(file_name, label_name, axs):
+    with open(file_name, 'rb') as f:
+        steps_run_list = np.load(f)
+
+    print(steps_run_list.shape)
+    mean_steps_run_list = np.mean(steps_run_list, axis=1)
+    std_steps_run_list = np.std(steps_run_list, axis=1)
+    x_steps_run_list = np.arange(steps_run_list.shape[2])
+
+    for stepsize_index in range(mean_steps_run_list.shape[0]):
+        drawPlotUncertainty(x_steps_run_list,
+                            mean_steps_run_list[stepsize_index],
+                            std_steps_run_list[stepsize_index],
+                            # label="MCTS(Baseline)" + str(stepsize_index),
+                            label=label_name,
+                            color=generate_hex_color(),
+                            axis=axs)
+
+
+def plot_alternate_agents(file_name, label_name1, label_name2, axs):
+    with open(file_name, 'rb') as f:
+        steps_run_list = np.load(f)
+
+    num_step = steps_run_list.shape[2] // 2
+    odd_ind = list(range(1, num_step * 2, 2))
+    even_ind = list(range(0, num_step * 2, 2))
+    agent1_step_list = np.delete(steps_run_list, odd_ind, 2)
+    agent2_step_list = np.delete(steps_run_list, even_ind, 2)
+
+    mean_agent1_steps_run_list = np.mean(agent1_step_list, axis=1)
+    std_agent1_steps_run_list = np.std(agent1_step_list, axis=1)
+    x_agent1_steps_run_list = np.arange(agent1_step_list.shape[2])
+
+    mean_agent2_steps_run_list = np.mean(agent2_step_list, axis=1)
+    std_agent2_steps_run_list = np.std(agent2_step_list, axis=1)
+    x_agent2_steps_run_list = np.arange(agent2_step_list.shape[2])
+
+    drawPlotUncertainty(x_agent1_steps_run_list,
+                        mean_agent1_steps_run_list[0],
+                        std_agent1_steps_run_list[0],
+                        label=label_name1,
+                        color=generate_hex_color(),
+                        axis=axs)
+
+    drawPlotUncertainty(x_agent2_steps_run_list,
+                        mean_agent2_steps_run_list[0],
+                        std_agent2_steps_run_list[0],
+                        label=label_name2,
+                        color=generate_hex_color(),
+                        axis=axs)
+
+
+fig, axs = plt.subplots(1, 1, constrained_layout=False)
+
+file_name = 'Results/MCTS_4by4_i30d75_num_steps_list.npy'
+label_name = 'MCTS'
+plot_simple_agent(file_name, label_name, axs)
+
+file_name = 'Results/DQNMCTS_InitialValue_4by4_num_steps_list.npy'
+label_name1 = '-'
+label_name2 = 'Initial Value'
+plot_alternate_agents(file_name, label_name1, label_name2, axs)
+
+file_name = 'Results/DQNMCTS_Bootstrap_4by4_num_steps_list.npy'
+label_name1 = '-'
+label_name2 = 'Bootstrap'
+plot_alternate_agents(file_name, label_name1, label_name2, axs)
+
+file_name = 'Results/DQNMCTS_BootstrapInitial_4by4_i30d75_keeptree_num_steps_list.npy'
+label_name1 = '-'
+label_name2 = 'Initial Value + Bootstrap'
+plot_alternate_agents(file_name, label_name1, label_name2, axs)
+
+axs.title.set_text("NOT Keep Tree Version")
+axs.legend()
+fig.show()
--- Main.py
+++ Main.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     show_values_grid = [False, False],
     show_model_error_grid = [False, False]
 
-    s_vf_list = [2 ** -5, 2 ** -7, 2 ** -9, 2** -11]
+    s_vf_list = [2 ** -5, 2 ** -7, 2 ** -9, 2 ** -11]
     s_md_list = [2 ** -9]
 
     c_list = [2**0.5]#[2 ** -1, 2 ** 0, 2**0.5, 2 ** 1]

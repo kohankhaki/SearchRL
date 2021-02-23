e942b2852854bcaa2bd55278ff9d9738d9c3fbcb
kohankhaki <kohankha@ualberta.ca>
2021-02-23 11:41:47 -0700

    DQNMCTS Selection - Debugged.

--- Analyze2.py
+++ Analyze2.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 from matplotlib import pyplot as plt
 import pickle
+import Config as config
 
 def generate_hex_color():
     r = random.randint(0, 255)
@@ -51,10 +52,10 @@ def plot_simple_agent_single_episode(steps_run_list, label_name, axs):
     best_par = np.argmin(mean_steps_run_list)
     # print(best_par)
     print("best parameter case: ", mean_steps_run_list[best_par])
-    for stepsize_index in range(mean_steps_run_list.shape[0]):
-        axs.axhline(mean_steps_run_list[stepsize_index], color=generate_hex_color(), label=label_name+str(stepsize_index))
-
-
+    for stepsize_index in range(1,2):
+    # for stepsize_index in range(mean_steps_run_list.shape[0]):
+        axs.axhline(mean_steps_run_list[stepsize_index], color=generate_hex_color(), label=label_name)
+        # axs.axhline(mean_steps_run_list[stepsize_index], color=generate_hex_color(), label=label_name+str(stepsize_index))
 
 def plot_alternate_agents(file_name, label_name1, label_name2, axs):
     with open(file_name, 'rb') as f:
@@ -88,42 +89,58 @@ def plot_alternate_agents(file_name, label_name1, label_name2, axs):
                         color=generate_hex_color(),
                         axis=axs)
 
-if __name__ == "__main__":
-    file_name = 'Results/DQN_ParameterStudy.p'
-    with open(file_name, "rb") as f:
-        res = pickle.load(f)
-    # print(res.keys())
-    # print(res['num_steps'][0].shape)
-    # print(res['experiment_objs'][0].num_iteration)
 
+def plot_alternate_agents_single_episode(steps_run_list, label_name1, label_name2, axs):
+
+
+    print(steps_run_list.shape)
+    num_step = config.episodes_only_dqn
+    print(num_step)
+    agent1_ind = list(range(0, num_step, 1))
+    agent2_ind = list(range(num_step, num_step + config.episodes_only_mcts, 1))
+    agent1_step_list = np.delete(steps_run_list, agent2_ind, 2)
+    agent2_step_list = np.delete(steps_run_list, agent1_ind, 2)
+    print(agent2_ind)
+
+    mean_agent1_steps_run_list = np.mean(agent1_step_list, axis=1)
+    std_agent1_steps_run_list = np.std(agent1_step_list, axis=1)
+    x_agent1_steps_run_list = np.arange(agent1_step_list.shape[2])
+
+    drawPlotUncertainty(x_agent1_steps_run_list,
+                        mean_agent1_steps_run_list[0],
+                        std_agent1_steps_run_list[0],
+                        label=label_name1,
+                        color=generate_hex_color(),
+                        axis=axs)
+
+    mean_agent2_steps_run_list = np.mean(agent2_step_list, axis=1)
+    mean_agent2_steps_run_list = np.mean(mean_agent2_steps_run_list, axis=1)
+
+    for stepsize_index in range(mean_agent2_steps_run_list.shape[0]):
+        axs.axhline(mean_agent2_steps_run_list[stepsize_index], color=generate_hex_color(), label=label_name2)
+
+if __name__ == "__main__":
     fig, axs = plt.subplots(1, 1, constrained_layout=False)
 
+    file_name = 'Results/DQNMCTS_BootstrapInitial.p'
+    with open(file_name, "rb") as f:
+        res = pickle.load(f)
     print(res['num_steps'])
     steps_run_list = res['num_steps']
-    label_name = 'DQN'
-    # plot_simple_agent_single_episode(steps_run_list, label_name, axs)
-    plot_simple_agent(steps_run_list, label_name, axs)
-    # file_name = 'Results/DQNMCTS_InitialValue_4by4_num_steps_list.npy'
-    # label_name1 = '-'
-    # label_name2 = 'Initial Value'
-    # plot_alternate_agents(file_name, label_name1, label_name2, axs)
-    #
-    # file_name = 'Results/DQNMCTS_Bootstrap_4by4_num_steps_list.npy'
-    # label_name1 = '-'
-    # label_name2 = 'Bootstrap'
-    # plot_alternate_agents(file_name, label_name1, label_name2, axs)
-    #
-    # file_name = 'Results/DQNMCTS_BootstrapInitial_4by4_i30d75_keeptree_num_steps_list.npy'
-    # label_name1 = '-'
-    # label_name2 = 'Initial Value + Bootstrap'
-    # plot_alternate_agents(file_name, label_name1, label_name2, axs)
+    label_name1 = 'DQN'
+    label_name2 = 'MCTS(BootstrapInitial)'
+    plot_alternate_agents_single_episode(steps_run_list, label_name1, label_name2, axs)
+
+    file_name = 'Results/MCTS_ParameterStudy.p'
+    with open(file_name, "rb") as f:
+        res = pickle.load(f)
+    print(res['num_steps'])
+    steps_run_list = res['num_steps']
+    label_name = 'MCTS'
+    plot_simple_agent_single_episode(steps_run_list, label_name, axs)
 
 
-    # file_name = 'Results/DQNMCTS_InitialValue_4by4_Offline_6464_num_steps_list.npy'
-    # label_name = 'Offline MCTS'
-    # plot_simple_agent_single_episode(file_name, label_name, axs)
-    #
-    axs.title.set_text("DQN Parameter Study")
+    axs.title.set_text("DQNMCTS BootstrapInitial")
     axs.legend()
-    fig.savefig("Results/Plots/DQN_ParameterStudy.png")
+    fig.savefig("Results/Plots/DQNMCTS_BootstrapInitial.png")
     fig.show()
